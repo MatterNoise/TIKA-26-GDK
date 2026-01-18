0e1bf3bb1823a787d67bcdea151ad60dcdda0659
MatterNoise <srmatternoisedev@gmail.com>
2026-01-18 15:14:42 -0500

    Added the function 'DrawSprite' in TikaDraw.lua

--- TikaDraw.lua
+++ TikaDraw.lua
@@ -8,6 +8,21 @@ function TikaDraw:DrawLine(get_line_start, get_line_end, get_line_color)
 	line(get_line_start.x, get_line_start.y, get_line_end.x, get_line_end.y, get_line_color)
 end
 
-function TikaDraw:DrawSprite(get_sprite_position)
+function TikaDraw:DrawSprite(get_sprite_id, get_sprite_position, get_sprite_colorkey, get_sprite_scale, get_sprite_flip, get_sprite_rotation, get_composite_width, get_composite_height)
+	local new_sprite_colorkey = get_sprite_colorkey or 0
+	local new_sprite_scale = get_sprite_scale or 1
+	local new_sprite_flip = get_sprite_flip or 0
+	local new_sprite_rotation = get_sprite_rotation or 0
+	local new_composite_width = get_composite_width or 1
+	local new_composite_height = get_composite_height or 1
 	
+	spr(get_sprite_id, get_sprite_position.x,
+	                   get_sprite_position.y,
+					   new_sprite_colorkey,
+					   new_sprite_scale,
+					   new_sprite_flip,
+					   new_sprite_rotation,
+					   new_composite_width,
+					   new_composite_height
+	)
 end
