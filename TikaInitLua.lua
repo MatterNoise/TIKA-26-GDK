a349c23d1837c45b64ea85401bcfdb5be47ea13b
MatterNoise <srmatternoisedev@gmail.com>
2026-04-06 10:35:22 -0500

    Add TikaClampValue

--- TikaInitLua.lua
+++ TikaInitLua.lua
@@ -7,10 +7,20 @@ local MATH_PI = math.pi
 
 local Random = math.random
 
+local Min = math.min
+local Max = math.max
+
 local Sqrt = math.sqrt
 local Cos = math.cos
 local Sin = math.sin
 
 function TikaDegreesToRadians(get_value)
 	return get_value * MATH_PI / 180
+end
+
+function TikaClampValue(get_value, get_min_value, get_max_value)
+	local new_min_value = get_min_value or 0
+	local new_max_value = get_max_value or 0
+	
+	return Max(Min(get_value, get_max_value), new_min_value)
 end
